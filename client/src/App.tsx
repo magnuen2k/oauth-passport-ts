d0e2c211d1d449961e74aef7326da64eb882bfb4
magnuen2k <maen030@student.kristiania.no>
2021-03-29 01:25:07 +0200

    create context to and fetch logged in user

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Homepage from "./Components/Homepage/Homepage";
 import LoginPage from "./Components/LoginPage/LoginPage";
 import NavBar from "./Components/NavBar/NavBar";
 import "./GlobalStyles.css";
+import { myContext } from "./Context";
 
 function App() {
+  const userObject = useContext(myContext);
+  console.log(userObject);
   return (
     <BrowserRouter>
       <NavBar />
---
+++ client/src/Context.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const myContext = createContext({});
+export default function Context(props: any) {
+  const [userObject, setUserObject] = useState<any>();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/getuser", { withCredentials: true })
+      .then((res) => {
+        if (res.data) {
+          setUserObject(res.data);
+        }
+      });
+  }, []);
+  return (
+    <div>
+      <myContext.Provider value={userObject}>
+        {props.children}
+      </myContext.Provider>
+    </div>
+  );
+}
