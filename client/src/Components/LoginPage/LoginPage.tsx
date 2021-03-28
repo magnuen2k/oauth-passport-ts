ad1bac68ff06a10d71decfd58127379eed3384c7
magnuen2k <maen030@student.kristiania.no>
2021-03-29 01:03:19 +0200

    add onclick to login button

--- client/src/Components/LoginPage/LoginPage.tsx
+++ client/src/Components/LoginPage/LoginPage.tsx
@@ -3,11 +3,14 @@ import googleImage from "../../assets/googleimage.png";
 import styles from "./Loginpage.module.css";
 
 export default function LoginPage() {
+  const googleLogin = () => {
+    window.open("http://localhost:4000/auth/google", "_self");
+  };
   return (
     <div className={styles.loginPage}>
       <div className={styles.loginForm}>
         <h1>Login</h1>
-        <div className={styles.googleContainer}>
+        <div className={styles.googleContainer} onClick={googleLogin}>
           <img src={googleImage} alt={"google logo"} />
           <p>Login with Google</p>
         </div>
