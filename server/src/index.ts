bb2e4a3d34abe50ad61e367d1ae431c98b1f2e0b
magnuen2k <maen030@student.kristiania.no>
2021-03-28 23:14:05 +0200

    setting up middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,40 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
 
 const app = express();
 
-mongoose.connect(process.env.DB_CONNECT, {
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}, () => {
+  },
+  () => {
     console.log("Database connected successfully!");
-})
+  }
+);
+
+//MIDDLEWARE
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
-    res.send("hello world");
-})
+  res.send("hello world");
+});
 
 app.listen(4000, () => {
-    console.log("Server started")
-})
+  console.log("Server started");
+});
