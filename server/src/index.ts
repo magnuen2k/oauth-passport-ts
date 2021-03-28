3d39dc00e3ef3a86b8fb4af37f9721543450497c
magnuen2k <maen030@student.kristiania.no>
2021-03-29 01:01:58 +0200

    setup google oauth with passport

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cors from "cors";
 import session from "express-session";
 import passport from "passport";
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
@@ -30,6 +31,42 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.serializeUser((user: any, done: any) => {
+  return done(null, user);
+});
+
+passport.deserializeUser((user: any, done: any) => {
+  return done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken: any, refreshToken: any, profile: any, cb: any) {
+      console.log(profile);
+      cb(null, profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("http://localhost:3000");
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("hello world");
