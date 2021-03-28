ca27800bc683088dcdc1d9aa1cd3208f8c149d8f
magnuen2k <maen030@student.kristiania.no>
2021-03-28 23:00:14 +0200

    connect to mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
+
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, () => {
+    console.log("Database connected successfully!");
+})
 
 app.get("/", (req, res) => {
     res.send("hello world");
