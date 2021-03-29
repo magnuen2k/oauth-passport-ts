ecae0bb4975ceceaed24b1fd437798ea6add0206
magnuen2k <maen030@student.kristiania.no>
2021-03-30 01:07:46 +0200

    configure cookie for https

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,11 +26,19 @@ mongoose.connect(
 //MIDDLEWARE
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+app.set("trust proxy", 1);
+
 app.use(
   session({
     secret: "secretcode",
     resave: true,
     saveUninitialized: true,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
   })
 );
 app.use(passport.initialize());
