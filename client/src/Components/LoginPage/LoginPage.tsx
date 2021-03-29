0893788a31cd7adadddbb6623ce332608d945a1d
magnuen2k <maen030@student.kristiania.no>
2021-03-29 11:22:54 +0200

    adding buttons for github and twitter

--- client/src/Components/LoginPage/LoginPage.tsx
+++ client/src/Components/LoginPage/LoginPage.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import googleImage from "../../assets/googleimage.png";
+import githubImage from "../../assets/githubImage.png";
+import twitterImage from "../../assets/twitterImage.png";
 import styles from "./Loginpage.module.css";
 
 export default function LoginPage() {
   const googleLogin = () => {
     window.open("http://localhost:4000/auth/google", "_self");
+  };
+
+  const githubLogin = () => {
+    // window.open("http://localhost:4000/auth/google", "_self");
+  };
+
+  const twitterLogin = () => {
+    // window.open("http://localhost:4000/auth/google", "_self");
   };
   return (
     <div className={styles.loginPage}>
@@ -13,6 +23,22 @@ export default function LoginPage() {
         <div className={styles.googleContainer} onClick={googleLogin}>
           <img src={googleImage} alt={"google logo"} />
           <p>Login with Google</p>
+        </div>
+
+        <div
+          className={`${styles.googleContainer} ${styles.githubContainer}`}
+          onClick={githubLogin}
+        >
+          <img src={githubImage} alt={"Github logo"} />
+          <p>Login with Github</p>
+        </div>
+
+        <div
+          className={`${styles.googleContainer} ${styles.twitterContainer}`}
+          onClick={twitterLogin}
+        >
+          <img src={twitterImage} alt={"Twitter logo"} />
+          <p>Login with Twitter</p>
         </div>
       </div>
     </div>
